e05a8bff51977e830beb15b3bea0765d258b8f79
Ivan Senitsev <39763078+IvanSenitsev@users.noreply.github.com>
2018-06-18 14:21:12 +0300

    ver01
    Yes

---
+++ PushToWin/PushToWin/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PushToWin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var but = (Button)sender;
+            but.Text = "You Win!!!";
+        }
+    }
+}
